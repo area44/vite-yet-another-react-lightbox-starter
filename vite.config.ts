6b896ee1587580ed29bc7a185f39435d69ec0b87
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 15:18:54 +0700

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
-
 import { defineConfig } from "vite";
+
+const base = process.env.BASE || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.CI
-    ? "/vite-yet-another-react-lightbox-starter/"
-    : undefined,
   resolve: {
     alias: {
       "@": path.resolve("./src"),
