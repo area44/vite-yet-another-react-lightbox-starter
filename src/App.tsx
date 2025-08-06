339a58de5c8571c02dcbe908e7006f85ae85e82a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-08-06 10:02:53 +0700

    refactor: update PhotoAlbum import and usage to RowsPhotoAlbum

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import * as React from "react";
-import PhotoAlbum from "react-photo-album";
+import { RowsPhotoAlbum } from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "react-photo-album/rows.css";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-import slides from "./data/slides";
+import { slides } from "@/data/slides";
 
 export default function PhotoGallery() {
   const [index, setIndex] = React.useState(-1);
 
   return (
     <>
-      <PhotoAlbum
-        layout="rows"
+      <RowsPhotoAlbum
         photos={slides}
         targetRowHeight={150}
-        onClick={({ index }) => setIndex(index)}
+        onClick={({ index: current }) => setIndex(current)}
       />
 
       <Lightbox
-        open={index >= 0}
         index={index}
-        close={() => setIndex(-1)}
         slides={slides}
+        open={index >= 0}
+        close={() => setIndex(-1)}
         plugins={[Thumbnails]}
       />
     </>
