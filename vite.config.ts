aeb03388554b40334e933ace665500815c7bea4b
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-10-24 14:51:43 +0700

    bump actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const base = process.env.base || "/";
+const base = process.env.BASE || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
