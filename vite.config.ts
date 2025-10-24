c20f246a2fbfdefdff70ef3898c1d4ac5eb27b23
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-10-24 14:29:55 +0700

    site var

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const base = process.env.BASE || "/";
+const base = process.env.base || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
